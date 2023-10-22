bc302c117f0f28ebd45816ad41fcfe629dfec9b6
maherrera3407 <herrerajaramillomiguelangel@gmail.com>
2023-10-21 19:40:19 -0500

    inicio reductor

--- metasapp/src/Componentes/lista/lista.js
+++ metasapp/src/Componentes/lista/lista.js
@@ -1,38 +1,12 @@
+import { useContext } from "react";
 import Meta from "./Meta";
-const listataMock=[{
-    'id':'1',
-    'detalles':'correr por 30 minutos',
-    'periodo':'dia',
-    'eventos':1,
-    'icono':'🏃',
-    'meta':365,
-    'plazo':'2030-01-01',
-    'completado':5
-},
-{
-    'id':'2',
-    'detalles':'leer libros',
-    'periodo':'año',
-    'eventos':6,
-    'icono':'📔',
-    'meta':12,
-    'plazo':'2030-01-01',
-    'completado':0
-},
-{
-    'id':'3',
-    'detalles':'Viajar',
-    'periodo':'mes',
-    'eventos':1,
-    'icono':'✈️',
-    'meta':60,
-    'plazo':'2030-01-01',
-    'completado':40
-}];
+import { Contexto } from "../servicios/Memoria";
+
 
 function Lista(){
+    const metas = useContext( Contexto);
     return( 
-       listataMock.map(meta => <Meta{...meta}></Meta>)
+       metas.map(meta => <Meta key={meta.id} {...meta}></Meta>)
     );
 }
 
--- metasapp/src/Componentes/servicios/Memoria.js
+++ metasapp/src/Componentes/servicios/Memoria.js
@@ -1,10 +1,48 @@
- import {createContext } from "react";
+ import {createContext, useReducer } from "react";
+
+
+ const listataMock=[{
+    'id':'1',
+    'detalles':'correr por 30 minutos',
+    'periodo':'dia',
+    'eventos':1,
+    'icono':'🏃',
+    'meta':365,
+    'plazo':'2030-01-01',
+    'completado':5
+},
+{
+    'id':'2',
+    'detalles':'leer libros',
+    'periodo':'año',
+    'eventos':6,
+    'icono':'📔',
+    'meta':12,
+    'plazo':'2030-01-01',
+    'completado':0
+},
+{
+    'id':'3',
+    'detalles':'Viajar',
+    'periodo':'mes',
+    'eventos':1,
+    'icono':'✈️',
+    'meta':60,
+    'plazo':'2030-01-01',
+    'completado':40
+}];
+
+const estadoInicial ={
+    orden:[],
+    objetos:{}
+};
 
  export const Contexto = createContext(null);
 
  function Memoria({children}) {
+    const [estado, enviar] = useReducer(reductor,estadoInicial);
     return (
-        <Contexto.Provider value={'compartido'}>
+        <Contexto.Provider value={[estado, enviar]}>
             {children}
         </Contexto.Provider>
     );
